b843b5917bf135eb9868a245b3862d094dc1d422
Dominik <dominik.merdzik@onet.pl>
2022-02-15 19:06:37 +0100

    added event listener on input in order to add tasks using enter on the keyboard

--- script.js
+++ script.js
@@ -1,90 +1,98 @@
 const btn = document.querySelector('.taskBtn');
+const input = document.querySelector('.inputTask');
 
-var counter = 1;
-var currentTaskNumber = 1;
+//was var
+let counter = 1;
+let currentTaskNumber = 1;
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', init);
+input.addEventListener('keyup', event => {
+    let enter = event.key;
+    if (enter === 'Enter') init();
+});
+
+//create a task and insert it to the list
+function init() {
     const providedTask = document.querySelector('.inputTask');
-    const taskList = document.querySelector('.task-list');
-    const listEl = document.createElement('li');
-    const div1 = document.createElement('div');
-    const span = document.createElement('span');
-    const btnWrap = document.createElement('div');
-    const deleteButton = document.createElement('button');
-    const iDel = document.createElement('i');
-    const iUp = document.createElement('i');
-    const iDown = document.createElement('i');
-    const upButton = document.createElement('button');
-    const downButton = document.createElement('button');
+        const taskList = document.querySelector('.task-list');
+        const listEl = document.createElement('li');
+        const div1 = document.createElement('div');
+        const span = document.createElement('span');
+        const btnWrap = document.createElement('div');
+        const deleteButton = document.createElement('button');
+        const iDel = document.createElement('i');
+        const iUp = document.createElement('i');
+        const iDown = document.createElement('i');
+        const upButton = document.createElement('button');
+        const downButton = document.createElement('button');
 
-    taskList.classList.add('task-list');
-    listEl.classList.add('list-element');
-    div1.classList.add('div-task');
-    span.classList.add('task');
-    btnWrap.classList.add('btn-wrap');
-    deleteButton.classList.add('delete-btn');
-    iDel.classList.add('fa-solid');
-    iDel.classList.add('fa-circle-minus');
-    upButton.classList.add('up-btn');
-    iUp.classList.add('fa-solid');
-    iUp.classList.add('fa-circle-arrow-up');
-    downButton.classList.add('down-btn');
-    iDown.classList.add('fa-solid');
-    iDown.classList.add('fa-circle-arrow-down');
+        taskList.classList.add('task-list');
+        listEl.classList.add('list-element');
+        div1.classList.add('div-task');
+        span.classList.add('task');
+        btnWrap.classList.add('btn-wrap');
+        deleteButton.classList.add('delete-btn');
+        iDel.classList.add('fa-solid');
+        iDel.classList.add('fa-circle-minus');
+        upButton.classList.add('up-btn');
+        iUp.classList.add('fa-solid');
+        iUp.classList.add('fa-circle-arrow-up');
+        downButton.classList.add('down-btn');
+        iDown.classList.add('fa-solid');
+        iDown.classList.add('fa-circle-arrow-down');
 
-    if(currentTaskNumber <= counter) {
-        span.textContent = currentTaskNumber + '. ' + providedTask.value;
-        currentTaskNumber = counter;
-    }
+        if(currentTaskNumber <= counter) {
+            span.textContent = currentTaskNumber + '. ' + providedTask.value;
+            currentTaskNumber = counter;
+        }
 
-    currentTaskNumber++;
-    counter++;
+        currentTaskNumber++;
+        counter++;
 
-    deleteButton.appendChild(iDel);
-    upButton.appendChild(iUp);
-    downButton.appendChild(iDown);
+        deleteButton.appendChild(iDel);
+        upButton.appendChild(iUp);
+        downButton.appendChild(iDown);
 
-    btnWrap.appendChild(deleteButton);
-    btnWrap.appendChild(upButton);
-    btnWrap.appendChild(downButton);
+        btnWrap.appendChild(deleteButton);
+        btnWrap.appendChild(upButton);
+        btnWrap.appendChild(downButton);
 
-    div1.appendChild(span);
-    div1.appendChild(btnWrap);
+        div1.appendChild(span);
+        div1.appendChild(btnWrap);
 
-    listEl.appendChild(div1);
+        listEl.appendChild(div1);
 
-    if (taskList.length === 0) {
-        taskList.appendChild(listEl);
-    } else {
-        taskList.insertBefore(listEl, taskList.firstElementChild);
-    }
+        if (taskList.length === 0) {
+            taskList.appendChild(listEl);
+        } else {
+            taskList.insertBefore(listEl, taskList.firstElementChild);
+        }
 
-    //functionality responsible for deleting currently selected task
-    deleteButton.addEventListener('click', () => {
-        currentTaskNumber = listEl.firstElementChild.textContent[0];
-        listEl.remove();
-        counter--;
-    })
+        //functionality responsible for deleting currently selected task
+        deleteButton.addEventListener('click', () => {
+            currentTaskNumber = listEl.firstElementChild.textContent[0];
+            listEl.remove();
+            counter--;
+        })
 
-    const parent = listEl.parentNode;
+        const parent = listEl.parentNode;
 
-    //functionality responsible for moving upwards selected task
-    upButton.addEventListener('click', () => {
-        const prev = listEl.previousElementSibling;
-        let oldChild = parent.removeChild(listEl);
-        parent.insertBefore( oldChild, prev );
-    })
+        //functionality responsible for moving upwards selected task
+        upButton.addEventListener('click', () => {
+            const prev = listEl.previousElementSibling;
+            let oldChild = parent.removeChild(listEl);
+            parent.insertBefore( oldChild, prev );
+        })
 
-    //functionality responsible for moving downwards selected task
-    downButton.addEventListener('click', () => {
-        const next = listEl.nextElementSibling;
-        let oldChild = parent.removeChild(listEl);
-        insertAfter( oldChild, next );
-    })
+        //functionality responsible for moving downwards selected task
+        downButton.addEventListener('click', () => {
+            const next = listEl.nextElementSibling;
+            let oldChild = parent.removeChild(listEl);
+            insertAfter( oldChild, next );
+        })
 
-    providedTask.value = '';
-
-})
+        providedTask.value = '';
+}
 
 //opposite of insertBefore function
 function insertAfter(newNode, existingNode) {
